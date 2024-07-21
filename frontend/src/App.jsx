5d2f8210304ff5fa914e526b2b5543dbb6323fe2
Elwin Roman <example>
2024-07-21 14:59:09 -0500

    chore(frontend): se elimina typescript

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      {/* code */}
+    </>
+  )
+}
+
+export default App
