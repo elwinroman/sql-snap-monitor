05b82d942a29c0f934a49937f04e349b648052c0
Elwin Roman <elwinromanv@gmail.com>
2024-07-26 01:16:06 -0500

    chore(fronted): deshabilita reglas

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -14,11 +14,9 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    'react/prop-types': 'off',
     'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': 'off',
     'prettier/prettier': ['error', {
       'printWidth': 80,
       'tabWidth': 2,
