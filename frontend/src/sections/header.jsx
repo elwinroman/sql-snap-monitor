97344f3fcfc160747865119118bfeebb8781a8bd
Elwin Roman <elwinromanv@gmail.com>
2024-07-27 23:39:16 -0500

    feat(frontend): agrega estilos a login y username
    chore: Instala componentes radixui (avatar, dropdown-menu)

--- frontend/src/sections/header.jsx
+++ frontend/src/sections/header.jsx
@@ -1,5 +1,16 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
 import { InputWithIcon } from '@/components/ui/input-with-icon'
 import { Search as SearchIcon } from '@/icons/search'
+import { User as UserIcon } from '@/icons/user'
 
 export function Header() {
   const handleKeyup = (e) => {
@@ -13,17 +24,62 @@ export function Header() {
       // Actualizar el estado del stringCode para renderizar el codigo
     }
   }
+
+  const logged = true
   return (
-    <header className="flex w-full flex-[0_0_var(--navbar-height)] flex-row items-center gap-8">
-      <div className="grow">1</div>
-      <div>
-        <InputWithIcon
-          startIcon={<SearchIcon />}
-          placeholder="Search"
-          onKeyUp={handleKeyup}
-        />
-      </div>
-      <div>aroman</div>
+    <header className="w-full flex-[0_0_var(--navbar-height)]">
+      <ul className="flex h-full flex-row items-center gap-4 px-4">
+        <li className="grow">
+          <div>1</div>
+        </li>
+
+        {/* Busqueda */}
+        <li>
+          <InputWithIcon
+            size="default"
+            startIcon={<SearchIcon width={20} height={20} />}
+            placeholder="Search"
+            onKeyUp={handleKeyup}
+          />
+        </li>
+
+        {/* Login y user */}
+        <li>
+          {!logged ? (
+            <Button variant="ghost" size="sm">
+              <div className="flex items-center gap-1">
+                <i>
+                  <UserIcon width={16} height={16} />
+                </i>
+                <span className="select-none text-sm font-semibold">
+                  Iniciar sesión
+                </span>
+              </div>
+            </Button>
+          ) : (
+            <div className="grid h-9 place-content-center px-3">
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <div className="flex items-center gap-2">
+                    <Avatar>
+                      <AvatarImage src="https://github.com/shadcn.png" />
+                      <AvatarFallback>QA</AvatarFallback>
+                    </Avatar>
+                    <span className="select-none text-sm font-semibold">
+                      aroman
+                    </span>
+                  </div>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="center" sideOffset={10}>
+                  <DropdownMenuLabel>Mi cuenta SQL</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>Cerrar sesión</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          )}
+        </li>
+      </ul>
       <div>icons</div>
     </header>
   )
