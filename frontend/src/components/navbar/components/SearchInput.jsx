15a422f6c5510b70eceecd4d0ed9f2804bd8365e
Elwin Roman <elwinromanv@gmail.com>
2024-08-03 23:14:31 -0500

    feat(frontend): deshabilita input en rutas

--- frontend/src/components/navbar/components/SearchInput.jsx
+++ frontend/src/components/navbar/components/SearchInput.jsx
@@ -1,31 +1,49 @@
 import { InputWithIcon } from '@/components/ui/input-with-icon'
 import { Search as SearchIcon } from '@/icons/search'
 import { useDefinition } from '@/hooks/useDefinition'
+import { useLocation } from 'react-router-dom'
 import { useObjectStore } from '@/stores/object.store'
+import { useEffect, useRef } from 'react'
 
 export function SearchInput() {
   const { getDefinitionObject } = useDefinition()
   const object = useObjectStore((state) => state.object)
+  const currentLocation = useLocation()
+  const inputBtn = useRef()
 
   const handleKeyup = async (e) => {
     e.preventDefault()
     const value = e.target.value.trim()
-    console.log('value', value, 'object.name', object.name)
     if (value === '') return
     if (object.name && value.toLowerCase() === object.name.toLowerCase()) return
 
     if (e.key === 'Enter') {
-      await getDefinitionObject({ name: value })
+      if (currentLocation.pathname === '/definition')
+        await getDefinitionObject({ name: value })
+      // else if (currentLocation.pathname === '/description')
+      //   await getDescriptionObject({ name: value })
     }
   }
+
+  // limpia el input y focus al cambiar de ruta
+  useEffect(() => {
+    inputBtn.current.focus()
+    inputBtn.current.value = ''
+  }, [currentLocation])
 
   return (
     <li>
       <InputWithIcon
+        ref={inputBtn}
         size="default"
         startIcon={<SearchIcon width={20} height={20} />}
         placeholder="Search"
         onKeyUp={handleKeyup}
+        disabled={
+          // disponible solo para obtener definiciones y descripciones
+          currentLocation.pathname !== '/definition' &&
+          currentLocation.pathname !== '/description'
+        }
       />
     </li>
   )
