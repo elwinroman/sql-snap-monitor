7a678f354699e20c389ba7504394a44a18ce7fc2
Elwin Roman <elwinromanv@gmail.com>
2024-08-04 04:04:13 -0500

    refactor(frontend): mueve info object a definition page

--- frontend/src/pages/Definition/components/BreadcrumCard.jsx
+++ frontend/src/pages/Definition/components/BreadcrumCard.jsx
@@ -6,19 +6,22 @@ import {
 } from '@/components/ui/tooltip'
 import { Badge } from '@/components/ui/badge'
 import { Database as DatabaseIcon } from '@/icons/database'
+import { useObjectStore } from '@/stores/object.store'
 
-export function InfoObject() {
+export function BreadcrumCard({ ...props }) {
+  const object = useObjectStore((state) => state.object)
+
   return (
-    <li className="grow">
-      <article className="flex w-fit gap-8 rounded-md bg-indigo-100 px-3 py-2 dark:bg-indigo-900">
-        <div className="flex gap-2">
+    <li {...props}>
+      <article className="flex h-full justify-between gap-8 rounded-md px-3 py-2">
+        <div className="flex gap-3">
           <TooltipProvider delayDuration={200}>
             <Tooltip>
               <TooltipTrigger className="flex items-center gap-1">
                 <i>
                   <DatabaseIcon width={18} height={18} />
                 </i>
-                <span className="font-semibold text-slate-800 dark:text-red-100">
+                <span className="font-semibold text-slate-100">
                   SI_BDFinanciero
                 </span>
               </TooltipTrigger>
@@ -31,8 +34,8 @@ export function InfoObject() {
           <TooltipProvider delayDuration={200}>
             <Tooltip>
               <TooltipTrigger className="grid place-content-center">
-                <span className="rounded-md border border-slate-400 px-1 pb-0.5 text-xs outline-none dark:border-indigo-200">
-                  dbo
+                <span className="rounded-md border border-amber-400 px-1 pb-0.5 text-xs text-amber-400 outline-none">
+                  {object.schema ?? 'dbo'}
                 </span>
               </TooltipTrigger>
               <TooltipContent>
@@ -44,8 +47,9 @@ export function InfoObject() {
           <TooltipProvider delayDuration={200}>
             <Tooltip>
               <TooltipTrigger className="grid place-content-center">
-                <span className="font-semibold text-rose-600 dark:text-rose-500">
-                  SI_FinCreditos
+                <span className="font-bold text-rose-600">
+                  {' '}
+                  {object.name ?? 'ObjectName'}
                 </span>
               </TooltipTrigger>
               <TooltipContent>
@@ -54,7 +58,9 @@ export function InfoObject() {
             </Tooltip>
           </TooltipProvider>
         </div>
-        <Badge className="rounded-md text-sm font-bold">P</Badge>
+        <Badge className="rounded-md bg-amber-400 text-sm font-bold text-slate-900 hover:bg-amber-400">
+          P
+        </Badge>
       </article>
     </li>
   )
