e391cd1def11842fb714b14c501ae5936d5a4b49
Elwin Roman <elwinromanv@gmail.com>
2024-08-05 02:07:53 -0500

    refactor(frontend): add dark mode for test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,16 @@ import { Footer } from '@/sections/footer'
 import { Main } from '@/components/main/Main'
 import { Navbar } from '@/components/navbar/Navbar'
 import { Sidebar } from '@/components/sidebar/Sidebar'
+import { useEffect } from 'react'
 
 function App() {
+  useEffect(() => {
+    document.documentElement.classList.add('dark')
+  }, [])
   return (
     <div className="App flex w-full">
       <Sidebar />
-      <main className="bg-owmain relative flex h-screen w-full flex-col overflow-y-auto px-4">
+      <main className="relative flex h-screen w-full flex-col overflow-y-auto bg-owmain px-4">
         <Navbar />
         <Main />
         <Footer />
