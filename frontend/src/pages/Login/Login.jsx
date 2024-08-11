736f3c47c50002e0679720390b0d60ade921a30f
Elwin Roman <elwinromanv@gmail.com>
2024-08-11 01:21:24 -0500

    feat(frontend): add loader

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,13 +1,17 @@
 import { AlertCircle as AlertCircleIcon } from '@/icons/alert-circle'
 import { Input } from './components/Input'
 import { Label } from './components/Label'
+import { Loader } from '@/components/loader/Loader'
 import { Navigate } from 'react-router-dom'
 import { useAuthStore } from '@/stores/auth.store'
+import { useState } from 'react'
 
 export function LoginPage() {
   const loginUser = useAuthStore((state) => state.loginUser)
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated)
   const errorAuth = useAuthStore((state) => state.errorAuth)
+
+  const [loading, setLoading] = useState(false)
 
   if (isAuthenticated) return <Navigate to="/" />
 
@@ -23,20 +27,29 @@ export function LoginPage() {
     }
 
     try {
+      setLoading(true)
       await loginUser({ credentials })
     } catch (err) {
       throw new Error(err)
+    } finally {
+      setLoading(false)
     }
   }
   return (
     <section className="flex h-screen w-full items-center bg-[#14161a]">
       <div className="m-auto flex w-[500px] flex-col gap-8 rounded-md bg-[#26282d] px-14 py-16">
+        {/* Loader */}
+        {loading && (
+          <Loader className="loader absolute left-1/2 top-1/2 opacity-80" />
+        )}
+
         <header className="flex flex-col gap-3">
           <h1 className="text-4xl font-semibold text-white">Iniciar sesión</h1>
           <p className="font-medium text-zinc-400">
             Hoy es un buen día para revisar tus SPs, Tablas, etc. Inicia sesión
             con tus credenciales SQL
           </p>
+
           {errorAuth && (
             <div className="flex gap-2 rounded-md border border-[#f53e7b59] bg-[#592e41] px-4 py-2">
               <i className="pt-0.5">
