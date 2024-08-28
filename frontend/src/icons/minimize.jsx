3e4a49b30bc5b3004bc506d2262a8fb7239aecae
Elwin Roman <elwinromanv@gmail.com>
2024-08-28 01:56:27 -0500

    fix(frontend): fix no icon load to options editor code component

---
+++ frontend/src/icons/minimize.jsx
@@ -0,0 +1,20 @@
+export function Minimize({ width = 24, height = 24 }) {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M15 19v-2a2 2 0 0 1 2 -2h2" />
+      <path d="M15 5v2a2 2 0 0 0 2 2h2" />
+      <path d="M5 15h2a2 2 0 0 1 2 2v2" />
+      <path d="M5 9h2a2 2 0 0 0 2 -2v-2" />
+    </svg>
+  )
+}
