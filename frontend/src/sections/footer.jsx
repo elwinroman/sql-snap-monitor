39c669d1a1e503722c99f45eb932315b06717248
Elwin Roman <elwinromanv@gmail.com>
2024-08-28 01:55:14 -0500

    feat(frontend): versioning, add version at footer

--- frontend/src/sections/footer.jsx
+++ frontend/src/sections/footer.jsx
@@ -1,11 +1,16 @@
+import { version } from '~/package.json'
+
 export function Footer() {
   return (
-    <footer className="flex h-20 shrink-0 items-center justify-center">
-      <p className="text-slate-600 dark:text-slate-300">
+    <footer className="flex h-20 shrink-0 flex-col items-center justify-center gap-2">
+      <p className="text-sm text-slate-600 dark:text-slate-300">
         <span>Hecho con </span>
         <span>❤️</span>
         <span> por Romeltek</span>
       </p>
+      <span className="font-[Menlo] text-xs text-slate-300">
+        v.{version} (beta)
+      </span>
     </footer>
   )
 }
