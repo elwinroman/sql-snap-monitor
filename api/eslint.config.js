ae35b57e9f2cecfa6d45cf570e2dd74bb012a79a
Elwin Roman <elwinromanv@gmail.com>
2024-08-30 23:39:28 -0500

    chore(api): change auto sort import

--- api/eslint.config.js
+++ api/eslint.config.js
@@ -1,41 +1,40 @@
 import pluginJs from '@eslint/js'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default [
-	{ ignores: ['dist/'] },
-	{ files: ['**/*.{js,mjs,cjs,ts}'] },
-	{ languageOptions: { globals: globals.node } },
-	pluginJs.configs.recommended,
-	...tseslint.configs.recommended,
-	eslintPluginPrettierRecommended,
-	{
-		rules: {
-			'prettier/prettier': [
-				'error',
-				{
-					printWidth: 140,
-					tabWidth: 2,
-					useTabs: true,
-					semi: false,
-					singleQuote: true,
-					trailingComma: 'all',
-					bracketSpacing: true,
-					arrowParens: 'avoid',
-					endOfLine: 'auto',
-				},
-			],
-			'sort-imports': [
-				'error',
-				{
-					ignoreCase: false,
-					ignoreDeclarationSort: true,
-					ignoreMemberSort: false,
-					memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-					allowSeparatedGroups: false,
-				},
-			],
-		},
-	},
+  { ignores: ['dist/'] },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+  },
+  {
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          printWidth: 140,
+          tabWidth: 2,
+          useTabs: false,
+          semi: false,
+          singleQuote: true,
+          trailingComma: 'all',
+          bracketSpacing: true,
+          arrowParens: 'avoid',
+          endOfLine: 'auto',
+          embeddedLanguageFormatting: 'off',
+        },
+      ],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
 ]
