d170ca829268ac9093337b5647257a6a5de21f35
Elwin Roman <elwinromanv@gmail.com>
2024-09-07 19:20:12 -0500

    refactor(api): update jwt middleware to add boolean session state

--- api/src/middlewares/jwt.ts
+++ api/src/middlewares/jwt.ts
@@ -1,26 +1,30 @@
 import { NextFunction, Request } from 'express'
+import type { JwtPayload } from 'jsonwebtoken'
 import jwt from 'jsonwebtoken'
 
-import { Credentials } from '@/models/schemas'
-/**
- * Verifica el token de acceso, y almacena el usuario en la sesión
- * (si no existe el token, la sesión se mantiene en null)
- *
- * @param {*} req
- * @param {*} res
- * @param {*} next
- */
+import { Credentials, CredentialsInitialState } from '@/models/schemas'
+
+interface TokenPayload extends JwtPayload {
+  credentials: Credentials
+}
+
+// Verifica el token de acceso, y almacena el usuario en la sesión
 export function verifyToken(req: Request, _res: Response, next: NextFunction) {
   const token = req.cookies.access_token
 
   req.session.isSessionActive = false // no existe usuario por defecto
+  req.session.credentials = { ...CredentialsInitialState }
   try {
-    const credentials = jwt.verify(token, process.env.JWT_SECRET as string) as Credentials
-    req.session.isSessionActive = true
-    req.session.credentials = { ...credentials } // se guarda el usuario en la sesión
+    const tokenDecoded = jwt.verify(token, process.env.JWT_SECRET as string)
+
+    if (typeof tokenDecoded === 'object' && tokenDecoded !== null && 'credentials' in tokenDecoded) {
+      const { credentials }: { credentials: Credentials } = tokenDecoded as TokenPayload
+      req.session.isSessionActive = true
+      req.session.credentials = { ...credentials } // se guarda el usuario en la sesión
+    }
   } catch {
     req.session.isSessionActive = false
-    req.session.credentials = null
+    req.session.credentials = CredentialsInitialState
   }
   next()
 }
---
+++ api/src/types/express-session.d.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import session from 'express-session'
+
+import { Credentials } from '@/models/schemas'
+
+declare module 'express-session' {
+  // Añade aquí las propiedades de sesión que necesites
+  interface SessionData {
+    credentials: Credentials
+    isSessionActive: boolean
+  }
+}
