be1c4179b0443b293388c68c3e269b228dce5088
Elwin Roman <elwinromanv@gmail.com>
2024-09-08 02:24:37 -0500

    feat(api): add jsdoc to RecordObjet

--- api/src/models/schemas/object.schema.ts
+++ api/src/models/schemas/object.schema.ts
@@ -1,5 +1,19 @@
 import { CustomError } from './custom-error.schema'
 
+// Para objetos que requiren obtener su definición SQL
+
+/**
+ * @typedef {Object} RecordObject
+ * @property {number} id - ID del objeto.
+ * @property {string} name - Nombre del objeto.
+ * @property {string} type - Tipo del objeto.
+ * @property {string} typeDesc - Descripción del tipo de objeto.
+ * @property {string} schema - Nombre del esquema. Es único en la base de datos.
+ * @property {number} schemaId - Identificador del esquema en el que se encuentra el objeto.
+ * @property {Date} createDate - Fecha de creación del objeto.
+ * @property {Date} modifyDate - Fecha en que el objeto se modificó por última vez mediante una ALTER instrucción.
+ * @property {string} definition - Texto SQL que define este módulo. NULL = Cifrado.
+ */
 export interface RecordObject {
   id: number
   name: string
