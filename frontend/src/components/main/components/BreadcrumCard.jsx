c390c87ec069c5b498d204303630132bf0c6e586
Elwin Roman <elwinromanv@gmail.com>
2024-09-13 00:50:17 -0500

    feat(add): improve to minimalist styles and add schema info

--- frontend/src/components/main/components/BreadcrumCard.jsx
+++ frontend/src/components/main/components/BreadcrumCard.jsx
@@ -6,17 +6,14 @@ export function BreadcrumCard({ object }) {
 
   return (
     <div className="flex items-baseline gap-6 border-b border-b-zinc-800 py-1">
-      <span className="text-3xl text-zinc-400">EXPLORANDO</span>
+      <span className="text-2xl text-zinc-400">EXPLORANDO</span>
       <div className="flex items-baseline gap-2">
-        <h1 className="text-xl font-bold text-slate-300">{dbname}</h1>
-
+        <h1 className="text-base font-medium text-slate-200">{dbname}</h1>
+        <span className="text-zinc-500">/</span>
         {object.id && (
-          <>
-            <span className="mt-0.5 self-center rounded-md bg-orange-600 px-1.5 pb-0.5 text-xs font-bold text-zinc-100 outline-none">
-              {object.schema}
-            </span>
-            <h2 className="text-xl font-bold text-amber-400">{object.name}</h2>
-          </>
+          <h2 className="text-base font-medium text-amber-400">
+            {object.name}
+          </h2>
         )}
       </div>
     </div>
--- frontend/src/components/main/components/InfoCards.jsx
+++ frontend/src/components/main/components/InfoCards.jsx
@@ -1,63 +1,62 @@
-import { Article as ArticleIcon } from '@/icons/article'
+// import { Article as ArticleIcon } from '@/icons/article'
 import { BreadcrumCard } from './BreadcrumCard'
-import { CalendarPlus as CalendarPlusIcon } from '@/icons/calendar-plus'
-import { CalendarStats as CalendarStatsIcon } from '@/icons/calendar-stats'
-import { formatLocalDate } from '@/utilities'
+// import { CalendarPlus as CalendarPlusIcon } from '@/icons/calendar-plus'
+// import { CalendarStats as CalendarStatsIcon } from '@/icons/calendar-stats'
 
 export function InfoCards({ object }) {
   const info = [
     {
       id: 1,
-      title: 'Tipo',
-      value: object.typeDesc ?? '-',
-      has_tag: true,
-      tag: object.type ? `(${object.type})` : '',
-      icon: <ArticleIcon width={22} height={22} />,
-      className: 'bg-cyan-400/20 text-cyan-400',
+      title: 'schema:',
+      value: object.schema ?? '',
+      className: 'bg-rose-400/20 text-rose-400',
     },
     {
       id: 2,
-      title: 'Fecha de creación',
-      value: object.createDate ? formatLocalDate(object.createDate) : '-',
-      icon: <CalendarPlusIcon width={22} height={22} />,
-      className: 'bg-emerald-500/20 text-emerald-500',
+      title: 'tipo:',
+      value: object.typeDesc ?? '',
+      has_tag: true,
+      tag: object.type ? `(${object.type})` : '',
+      className: 'bg-cyan-400/20 text-cyan-400',
     },
     {
       id: 3,
-      title: 'Fecha de modificación',
-      value: object.modifyDate ? formatLocalDate(object.modifyDate) : '-',
-      icon: <CalendarStatsIcon width={22} height={22} />,
+      title: 'fecha de creación:',
+      value: object.createDate ?? '',
+      className: 'bg-emerald-500/20 text-emerald-500',
+    },
+    {
+      id: 4,
+      title: 'fecha de modificación:',
+      value: object.modifyDate ?? '',
       className: 'bg-indigo-400/20 text-indigo-400',
     },
   ]
+
+  console.log(object)
 
   return (
     <div className="flex flex-col gap-5">
       {/* Información general del objecto */}
       <BreadcrumCard object={object} />
 
-      <ul className="grid auto-rows-fr grid-cols-[repeat(auto-fill,_minmax(310px,_1fr))] gap-5">
+      <ul className="flex gap-5">
         {info.map((item) => (
           <li
             key={item.id}
-            className="flex items-center justify-between gap-6 rounded-md border border-owborder bg-owcard px-6 py-4"
+            className="flex items-center justify-between gap-6 rounded-md border border-owborder bg-owcard px-3 py-2"
           >
-            <div className="flex flex-col gap-1">
-              <h6 className="text-sm font-semibold text-zinc-600 dark:text-zinc-400">
+            <div className="flex items-baseline gap-2">
+              <h6 className="text-xs font-normal text-zinc-600 dark:text-zinc-400">
                 {item.title}
               </h6>
-              <p className="dark:text-zinc-40000 text-base font-bold text-slate-300">
+              <p className="text-xs font-medium text-slate-300 dark:text-zinc-100">
                 <span>{item.value} </span>
                 {item.has_tag && (
-                  <span className="text-base text-amber-400">{item.tag}</span>
+                  <span className="text-xs text-amber-400">{item.tag}</span>
                 )}
               </p>
             </div>
-            <i
-              className={`grid place-content-center rounded-md p-2 ${item.className}`}
-            >
-              {item.icon}
-            </i>
           </li>
         ))}
       </ul>
