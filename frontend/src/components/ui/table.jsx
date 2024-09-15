5dd1254c809d0ed53c1ddb42d5e71eeea6604d81
Elwin Roman <elwinromanv@gmail.com>
2024-09-15 03:28:05 -0500

    refactor(frontend): style table

--- frontend/src/components/ui/table.jsx
+++ frontend/src/components/ui/table.jsx
@@ -43,7 +43,7 @@ const TableRow = React.forwardRef(({ className, ...props }, ref) => (
   <tr
     ref={ref}
     className={cn(
-      'border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted',
+      'border-b border-zinc-700 transition-colors hover:bg-zinc-700 data-[state=selected]:bg-muted',
       className,
     )}
     {...props}
@@ -55,7 +55,7 @@ const TableHead = React.forwardRef(({ className, ...props }, ref) => (
   <th
     ref={ref}
     className={cn(
-      'h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0',
+      'h-12 bg-zinc-700 px-4 text-left align-middle font-medium text-white [&:has([role=checkbox])]:pr-0',
       className,
     )}
     {...props}
--- frontend/src/pages/Description/components/TableDescription.jsx
+++ frontend/src/pages/Description/components/TableDescription.jsx
@@ -17,21 +17,39 @@ export function TableDescription({ userTableColumnList }) {
 
   return (
     <div>
-      <div className="py-2">
+      <div className="py-4">
         {userTableExtendedPropertieList.length > 0 &&
           userTableExtendedPropertieList.map((item) => (
             <div
               key={uuidv4()}
-              className="flex gap-5 rounded-sm border border-dashed border-slate-500 p-2"
+              className="flex gap-5 rounded-sm border border-dashed border-zinc-500 px-4 py-2"
             >
-              <p className="text-md text-slate-300">
-                <span className="font-medium">{item.propertyValue} </span>
-                <span className="rounded-sm bg-zinc-700 px-2 py-1 text-xs font-bold">
+              <p className="flex items-center gap-4 text-sm">
+                <span className="font-medium text-zinc-200">
+                  {item.propertyValue}
+                </span>
+                <span className="w-fit rounded-sm bg-slate-700 px-2 py-1 text-center text-xs text-white">
                   {item.propertyName}
                 </span>
               </p>
             </div>
           ))}
+
+        {userTableExtendedPropertieList.length === 0 && (
+          <div
+            key={uuidv4()}
+            className="flex gap-5 rounded-sm border border-dashed border-zinc-500 px-4 py-2"
+          >
+            <p className="flex items-center gap-4 text-sm">
+              <span className="text-zinc-500">
+                No existe descripción de tabla
+              </span>
+              <span className="text-zinc-500">
+                No existe nombre de propiedad
+              </span>
+            </p>
+          </div>
+        )}
       </div>
       <Table>
         <TableCaption>Lista de descripción de un objeto.</TableCaption>
@@ -50,14 +68,51 @@ export function TableDescription({ userTableColumnList }) {
           <TableBody>
             {userTableColumnList.map((item, index) => (
               <TableRow key={uuidv4()}>
-                <TableCell className="w-[20px]">{index + 1}</TableCell>
-                <TableCell>{item.name}</TableCell>
-                <TableCell>{item.type}</TableCell>
-                <TableCell>{item.extendedProperties[0].propertyValue}</TableCell>
+                <TableCell className="w-[20px] text-zinc-200">
+                  {index + 1}
+                </TableCell>
                 <TableCell>
-                  <span className="inline-block rounded-sm bg-zinc-700 px-2 py-1 text-center text-xs font-bold">
-                    {item.extendedProperties[0].propertyName}
+                  <span className="text-zinc-200">{item.name}</span>
+                </TableCell>
+                <TableCell>
+                  <span className="w-fit rounded-sm border border-zinc-600 bg-zinc-700 px-1 text-center text-xs text-zinc-200">
+                    {item.type}
                   </span>
+                </TableCell>
+                <TableCell>
+                  <div className="flex flex-col gap-2 text-zinc-200">
+                    {item.extendedProperties.length > 0 &&
+                      item.extendedProperties.map((subitem) => (
+                        <span key={uuidv4()} className="">
+                          {subitem.propertyValue}
+                        </span>
+                      ))}
+
+                    {item.extendedProperties.length === 0 && (
+                      <span className="text-zinc-500">
+                        No existe descripción
+                      </span>
+                    )}
+                  </div>
+                </TableCell>
+                <TableCell>
+                  <div className="flex flex-col gap-2">
+                    {item.extendedProperties.length > 0 &&
+                      item.extendedProperties.map((subitem) => (
+                        <span
+                          key={uuidv4()}
+                          className="w-fit rounded-sm bg-slate-700 px-2 py-1 text-center text-xs text-white"
+                        >
+                          {subitem.propertyName}
+                        </span>
+                      ))}
+
+                    {item.extendedProperties.length === 0 && (
+                      <span className="text-zinc-500">
+                        No existe nombre de propiedad
+                      </span>
+                    )}
+                  </div>
                 </TableCell>
                 <TableCell className="w-[20px]">{item.id}</TableCell>
               </TableRow>
