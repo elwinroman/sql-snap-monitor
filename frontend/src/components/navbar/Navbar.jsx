7cc7dbda998ab33df23d691c68464ea4d5853276
Elwin Roman <elwinromanv@gmail.com>
2024-09-26 01:10:04 -0500

    feat(frontend): add sticky navbar

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -4,11 +4,33 @@ import { LoginUsername } from './components/LoginUsername'
 import { SearchInput } from './components/SearchInput'
 import { NavMenu } from './components/NavMenu'
 import { useLocation } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import './styles/navbar.css'
 
 export function Navbar() {
   const currentLocation = useLocation()
+  const [isFixed, setIsFixed] = useState(false)
+
+  useEffect(() => {
+    const App = document.querySelector('.App')
+
+    const handleScroll = () => {
+      App.scrollTop > 80 ? setIsFixed(true) : setIsFixed(false)
+    }
+
+    window.addEventListener('scroll', handleScroll, true)
+
+    // Limpiar el evento al desmontar el componente
+    return () => {
+      App.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
-    <header className="w-full flex-[0_0_var(--navbar-height)]">
+    <header
+      id="navbar"
+      className={`w-full flex-[0_0_var(--navbar-height)] transition-all ${isFixed ? 'fixed top-0 z-50 max-w-screen-2xl py-4 backdrop-blur-[4px]' : 'static'}`}
+    >
       <ul className="flex h-full flex-row items-center gap-4 px-4">
         {/* Hamburguer menu */}
         <HamburguerMenu />
