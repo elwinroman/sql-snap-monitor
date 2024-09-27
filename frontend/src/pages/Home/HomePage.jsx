85a7655f45e4d6fc1d51de7934ee58bcfd8199d6
Elwin Roman <elwinromanv@gmail.com>
2024-09-27 17:25:34 -0500

    refactor(frontend): rename software name

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -5,11 +5,11 @@ export function HomePage() {
         <div className="flex flex-col items-center space-y-4 text-center">
           <div className="space-y-8">
             <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-              Optimiza tu gestión SQL, Aladdin está para ayudarte.
+              Optimiza tu gestión SQL, Omnissiah está para ayudarte.
             </h1>
             <p className="mx-auto max-w-[700px] text-gray-500 dark:text-gray-400 md:text-lg">
-              Aladdin es una herramienta de gestión de bases de datos SQL que te ayuda a optimizar tu trabajo como QA Engineer, recuperando
-              definiciones SQL, información de tablas de usuario, comparación de SPs, rastreo de claves foraneas y más.
+              Omnissiah es una herramienta de gestión de bases de datos SQL que te ayuda a optimizar tu trabajo como QA Engineer,
+              recuperando definiciones SQL, información de tablas de usuario, comparación de SPs, rastreo de claves foraneas y más.
             </p>
           </div>
           <div className="space-x-4"></div>
