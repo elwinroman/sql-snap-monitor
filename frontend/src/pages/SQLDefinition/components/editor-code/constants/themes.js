dda1501bb39b15f6ce0d444a42ebe2299d3d9482
Elwin Roman <elwinromanv@gmail.com>
2024-10-10 04:15:38 -0500

    feat(frontend): add themes json

---
+++ frontend/src/pages/SQLDefinition/components/editor-code/constants/themes.js
@@ -0,0 +1,34 @@
+import * as Themes from '../themes'
+
+export const THEMES = [
+  {
+    name: 'Dracula',
+    tag: 'dracula',
+    json: Themes.Dracula,
+  },
+  {
+    name: 'Amy',
+    tag: 'amy',
+    json: Themes.Amy,
+  },
+  {
+    name: 'Github Dark',
+    tag: 'githubdark',
+    json: Themes.GithubDark,
+  },
+  {
+    name: 'Vibrant Ink',
+    tag: 'vibrantink',
+    json: Themes.VibrantInk,
+  },
+  {
+    name: 'Night Owl',
+    tag: 'nightowl',
+    json: Themes.NightOwl,
+  },
+  {
+    name: 'Blackboard',
+    tag: 'blackboard',
+    json: Themes.Blackboard,
+  },
+]
---
+++ frontend/src/pages/SQLDefinition/components/editor-code/themes/index.js
@@ -0,0 +1,8 @@
+import Amy from './amy.theme'
+import Blackboard from './blackboard.theme'
+import Dracula from './dracula.theme'
+import GithubDark from './github-dark.theme'
+import NightOwl from './night-owl.theme'
+import VibrantInk from './vibrant-ink.theme'
+
+export { Amy, Blackboard, Dracula, GithubDark, NightOwl, VibrantInk }
