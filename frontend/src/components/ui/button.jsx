d9a0f41d6820d8f33b8422f6fb32628c21f5676e
Elwin Roman <elwinromanv@gmail.com>
2024-11-18 04:01:07 -0500

    style(frontend): cambia border-radius

--- frontend/src/components/ui/button.jsx
+++ frontend/src/components/ui/button.jsx
@@ -1,11 +1,11 @@
 import { Slot } from '@radix-ui/react-slot'
 import { cva } from 'class-variance-authority'
-import * as React from 'react'
+import { forwardRef } from 'react'
 
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-sm text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
@@ -18,8 +18,8 @@ const buttonVariants = cva(
       },
       size: {
         default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
+        sm: 'h-9 rounded-sm px-3',
+        lg: 'h-11 rounded-sm px-8',
         icon: 'h-10 w-10',
       },
     },
@@ -30,7 +30,7 @@ const buttonVariants = cva(
   },
 )
 
-const Button = React.forwardRef(({ className, variant, size, asChild = false, ...props }, ref) => {
+const Button = forwardRef(({ className, variant, size, asChild = false, ...props }, ref) => {
   const Comp = asChild ? Slot : 'button'
   return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
 })
--- frontend/src/components/ui/input-with-icon.jsx
+++ frontend/src/components/ui/input-with-icon.jsx
@@ -12,7 +12,7 @@ const InputWithIcon = forwardRef(({ className, type, endIcon, handleClick, isFoc
       <input
         type={type}
         className={cn(
-          `flex h-10 w-24 rounded-sm border bg-ownavbar px-4 py-2 text-sm file:border-0 file:bg-transparent file:text-base file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-5 sm:w-72 md:w-80 lg:w-96 ${isFocus ? 'border-amber-400' : 'border-owborder'}`,
+          `flex h-10 w-full rounded-sm border bg-background px-4 py-2 text-sm file:border-0 file:bg-transparent file:text-base file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-5 ${isFocus ? 'border-amber-400' : 'border-owborder'}`,
           endIcon ? 'pr-11' : '',
           className,
         )}
