2447a335f148fa46ea47d44a4b24030c67c94718
Elwin Roman <elwinromanv@gmail.com>
2024-12-04 15:18:00 -0500

    feat(frontend): mejora de tabla a datatable (pendiente filter search)

--- frontend/src/pages/Usertable/UsertablePage.jsx
+++ frontend/src/pages/Usertable/UsertablePage.jsx
@@ -8,16 +8,16 @@ import { ObjectDetails } from '@/components/main/ObjectDetails'
 import { Toaster } from '@/components/ui/sonner'
 import { useUserTableStore } from '@/stores'
 
-import { TableDescription } from './components'
-// import { DataTable } from './components/DataTable'
-// import { columns } from './constants/columns'
+import { Columns } from './components/Columns'
+import { DataTable } from './components/DataTable'
 
 export function UsertablePage() {
   const object = useUserTableStore((state) => state.userTableObject)
-  // const columns = useUserTableStore((state) => state.userTableColumnList)
   const error = useUserTableStore((state) => state.userTableError)
   const updateError = useUserTableStore((state) => state.updateUsertableError)
   const loading = useUserTableStore((state) => state.loading)
+
+  const headerObjectName = object.name ? `${object.schema}.${object.name}` : 'Tabla de usuario'
 
   // Maneja los errores, muestra y limpia la notificación al buscar un objeto QSL
   useEffect(() => {
@@ -31,19 +31,18 @@ export function UsertablePage() {
 
   return (
     <>
-      <ObjectDetails object={object} />
+      {object.id && <ObjectDetails object={object} />}
 
       <div className="flex flex-col gap-2 rounded-md border border-border bg-card px-6 py-6">
         <h4 className="flex items-center gap-2 pb-2 text-base font-medium text-zinc-300">
-          <span className="text-primary">{`${object.schema}.${object.name}` ?? 'Tabla de usuario'}</span>
+          <span className="text-primary">{headerObjectName}</span>
         </h4>
 
         {/* Alerta de error */}
         {error && <AlertMessages message={error} type="error" />}
 
         {/* Descripción del usertable */}
-        <TableDescription />
-        {/* <DataTable headerColumns={columns} /> */}
+        <DataTable headerColumns={Columns} />
       </div>
 
       <Toaster position="bottom-right" />
---
+++ frontend/src/pages/Usertable/components/DataTable.jsx
@@ -0,0 +1,96 @@
+import { flexRender, getCoreRowModel, getFilteredRowModel, getSortedRowModel, useReactTable } from '@tanstack/react-table'
+import { useMemo, useState } from 'react'
+
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+import { useUserTableStore } from '@/stores'
+
+export function DataTable({ headerColumns }) {
+  const [sorting, setSorting] = useState([])
+  // const [columnFilters, setColumnFilters] = useState([])
+
+  const columns = useUserTableStore((state) => state.userTableColumnList)
+  const indexes = useUserTableStore((state) => state.userTableIndexList)
+  const foreignKeys = useUserTableStore((state) => state.userTableForeignKeyList)
+
+  // use usa useMemo para evitar loop de renderización infinita
+  const formattedData = useMemo(() => {
+    return columns.map((item, index) => {
+      const foreignKey = foreignKeys.filter((element) => element.columnId === item.id)
+
+      return {
+        nro: index + 1,
+        nombre: {
+          desc: item.name,
+          renderPrimaryKeyIcon: indexes.find((element) => element.columnId === item.id)?.isPrimaryKey ?? false,
+          renderUniqueKeyIcon: indexes.find((element) => element.columnId === item.id)?.isUnique ?? false,
+        },
+        tipo: item.type,
+        nulo: item.isNullable,
+        atributo: item.isIdentity ? 'IDENTITY' : item.defaultDefinition ? `DEFAULT: ${item.defaultDefinition.slice(1, -1)}` : '',
+        referencia: [...foreignKey],
+        descripcion: item.extendedProperties.map((element) => element.propertyValue),
+        propriedad: item.extendedProperties.map((element) => element.propertyName),
+      }
+    })
+  }, [columns, foreignKeys, indexes])
+
+  const table = useReactTable({
+    data: formattedData,
+    columns: headerColumns,
+    getCoreRowModel: getCoreRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    // onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      sorting,
+      // columnFilters,
+    },
+  })
+
+  return (
+    <div className="rounded-md border">
+      {/* <div className="flex items-center py-4">
+        <input
+          placeholder="Filter emails..."
+          value={table.getColumn('nombre')?.getFilterValue() ?? ''}
+          onChange={(event) => table.getColumn('nombre')?.setFilterValue(event.target.value)}
+          className="max-w-sm"
+        />
+      </div> */}
+
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id} style={{ width: `${header.getSize()}px` }}>
+                    {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                  </TableHead>
+                )
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  )
+}
