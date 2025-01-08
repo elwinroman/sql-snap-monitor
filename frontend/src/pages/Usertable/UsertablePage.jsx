dce07837023f53ec52297ef00a96a8c4d870b118
Elwin Roman <elwinromanv@gmail.com>
2025-01-07 23:29:26 -0500

    feat(frontend): agrega descripción de tabla de usuario

--- frontend/src/pages/Usertable/UsertablePage.jsx
+++ frontend/src/pages/Usertable/UsertablePage.jsx
@@ -1,10 +1,12 @@
 // import { Table } from 'lucide-react'
+import { MessageCircleWarning } from 'lucide-react'
 import { useEffect } from 'react'
 import { toast } from 'sonner'
 
 import { AlertMessages } from '@/components/alert-messages/AlertMessages'
 import { LoaderSlack } from '@/components/loader/loader-slack/LoaderSlack'
 import { ObjectDetails } from '@/components/main/ObjectDetails'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 import { Toaster } from '@/components/ui/sonner'
 import { useUserTableStore } from '@/stores'
 
@@ -13,6 +15,7 @@ import { DataTable } from './components/DataTable'
 
 export function UsertablePage() {
   const object = useUserTableStore((state) => state.userTableObject)
+  const tableDescription = useUserTableStore((state) => state.userTableExtendedPropertieList)
   const error = useUserTableStore((state) => state.userTableError)
   const updateError = useUserTableStore((state) => state.updateUsertableError)
   const loading = useUserTableStore((state) => state.loading)
@@ -33,10 +36,31 @@ export function UsertablePage() {
     <>
       {object.id && <ObjectDetails object={object} />}
 
-      <div className="shadow-custom-card flex flex-col gap-2 rounded-md bg-card px-6 py-6">
+      <div className="flex flex-col gap-2 px-6 py-6 rounded-md bg-card shadow-custom-card">
         <h4 className="flex items-center gap-2 pb-2 text-base font-medium">
           <span className="text-primary">{headerObjectName}</span>
         </h4>
+
+        <div className="flex items-baseline justify-between gap-2 px-3 py-2 border border-dashed rounded-sm w-fit border-border">
+          <div className="flex flex-col gap-2 text-sm text-secondary">
+            {tableDescription.length > 0 ? (
+              tableDescription.map((item, index) => <span key={index}>{item.propertyValue} </span>)
+            ) : (
+              <span className="text-muted">No existe una descripción para esta tabla de usuario</span>
+            )}
+          </div>
+
+          {tableDescription.length > 1 && (
+            <Popover>
+              <PopoverTrigger>
+                <i className="grid place-items-center rounded-sm p-1.5 text-red-400 transition-colors hover:bg-action-hover">
+                  <MessageCircleWarning size={14} />
+                </i>
+              </PopoverTrigger>
+              <PopoverContent>¡Existe más de una descripción para esta tabla de usuario!</PopoverContent>
+            </Popover>
+          )}
+        </div>
 
         {/* Alerta de error */}
         {error && <AlertMessages message={error} type="error" />}
