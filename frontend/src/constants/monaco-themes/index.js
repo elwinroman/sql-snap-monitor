e1adf72f38c6cf546cc616a0d120a0f322494f43
Elwin Roman <elwinromanv@gmail.com>
2025-03-20 00:10:06 -0500

    feat(frontend): agrega estilos de monaco-editor

--- frontend/src/constants/monaco-themes/index.js
+++ frontend/src/constants/monaco-themes/index.js
@@ -1,8 +1,35 @@
 import Amy from './amy.theme'
 import Blackboard from './blackboard.theme'
+import BrillianceBlack from './brilliance-black.theme'
+import ChromeDevTools from './chrome-dev-tools.theme'
 import Dracula from './dracula.theme'
+import Eiffel from './eiffel.theme.json'
 import GithubDark from './github-dark.theme'
+import iPlastic from './i-plastic.theme'
+import Katzenmilch from './katzenmilch.theme'
 import NightOwl from './night-owl.theme'
+import Nord from './nord.theme'
+import OceanicNext from './oceanic-next.theme'
+import SolarizedLight from './solarized-light.theme'
+import TomorrowNightBlue from './tomorrow-night-blue.theme'
+import TomorrowNightEighties from './tomorrow-night-eighties.theme'
 import VibrantInk from './vibrant-ink.theme'
 
-export { Amy, Blackboard, Dracula, GithubDark, NightOwl, VibrantInk }
+export {
+  Amy,
+  Blackboard,
+  BrillianceBlack,
+  ChromeDevTools,
+  Dracula,
+  Eiffel,
+  GithubDark,
+  iPlastic,
+  Katzenmilch,
+  NightOwl,
+  Nord,
+  OceanicNext,
+  SolarizedLight,
+  TomorrowNightBlue,
+  TomorrowNightEighties,
+  VibrantInk,
+}
--- frontend/src/constants/themes.js
+++ frontend/src/constants/themes.js
@@ -31,4 +31,54 @@ export const THEMES = [
     tag: 'blackboard',
     json: Themes.Blackboard,
   },
+  {
+    name: 'BrillianceBlack',
+    tag: 'brillianceblack',
+    json: Themes.BrillianceBlack,
+  },
+  {
+    name: 'ChromeDevTools',
+    tag: 'chromedevtools',
+    json: Themes.ChromeDevTools,
+  },
+  {
+    name: 'Eiffel',
+    tag: 'eiffel',
+    json: Themes.Eiffel,
+  },
+  {
+    name: 'Katzenmilch',
+    tag: 'katzenmilch',
+    json: Themes.Katzenmilch,
+  },
+  {
+    name: 'Nord',
+    tag: 'nord',
+    json: Themes.Nord,
+  },
+  {
+    name: 'OceanicNext',
+    tag: 'oceanicnext',
+    json: Themes.OceanicNext,
+  },
+  {
+    name: 'SolarizedLight',
+    tag: 'solarizedlight',
+    json: Themes.SolarizedLight,
+  },
+  {
+    name: 'TomorrowNightBlue',
+    tag: 'tomorrownightblue',
+    json: Themes.TomorrowNightBlue,
+  },
+  {
+    name: 'TomorrowNightEighties',
+    tag: 'tomorrownighteighties',
+    json: Themes.TomorrowNightEighties,
+  },
+  {
+    name: 'iPlastic',
+    tag: 'iplastic',
+    json: Themes.iPlastic,
+  },
 ]
