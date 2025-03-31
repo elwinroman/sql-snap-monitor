7241b98a0a43e967f3ed5b22568fa21fbe462f8a
Elwin Roman <elwinromanv@gmail.com>
2025-03-30 23:41:21 -0500

    refactor(api): renombra atributos de user

--- api/src/modules/auth-users/domain/user.ts
+++ api/src/modules/auth-users/domain/user.ts
@@ -2,12 +2,12 @@ import { randomUUID } from 'crypto'
 
 export interface PrimitiveUser {
   id?: number
-  hashUsernameUID: string //user_uuid
+  hashId: string
   user: string
   server: string
   aliasServer: string
-  // createdAt: Date
-  // active: boolean
+  createdAt?: Date
+  active?: boolean
 }
 
 export class User {
@@ -16,20 +16,24 @@ export class User {
   static create(createUser: { user: string; server: string; aliasServer: string }): User {
     return new User({
       id: undefined,
-      hashUsernameUID: randomUUID(),
+      hashId: randomUUID(),
       user: createUser.user,
       server: createUser.server,
       aliasServer: createUser.aliasServer,
+      createdAt: new Date(),
+      active: true,
     })
   }
 
   toValue(): PrimitiveUser {
     return {
       id: this.atributes.id,
-      hashUsernameUID: this.atributes.hashUsernameUID,
+      hashId: this.atributes.hashId,
       user: this.atributes.user,
       server: this.atributes.server,
       aliasServer: this.atributes.aliasServer,
+      createdAt: this.atributes.createdAt,
+      active: this.atributes.active,
     }
   }
 }
