d75ff57d1dc0c0e74b863afb438dd30db3f4d1f7
Elwin Roman <elwinromanv@gmail.com>
2025-04-24 00:49:41 -0500

    chore(api): migración de encriptador a shared

---
+++ api/src/modules/shared/utils/cryptocode.util.ts
@@ -0,0 +1,93 @@
+/**
+ *
+ * Encriptador/desencriptador basado en criptocode (python package)
+ * https://github.com/gdavid7/cryptocode
+ *
+ */
+
+import crypto from 'node:crypto'
+
+// import { DecryptionException } from '@shared/domain/exceptions'
+import { PASS_PHRASE } from '@/config/enviroment'
+import { logger } from '@/modules/shared/infrastructure/pino-instance'
+
+const passPhrase = PASS_PHRASE
+
+interface ScryptOptions {
+  N: number // trabajo (CPU/memoria)
+  r: number // bloques
+  p: number // paralelismo
+  keyLen: number // longitud de la clave
+}
+
+class CryptoCode {
+  private AES_BLOCK_SIZE: number
+  private SCRYPT_OPTIONS: ScryptOptions
+
+  constructor() {
+    this.AES_BLOCK_SIZE = 16 // tamaño del bloque para AES
+    this.SCRYPT_OPTIONS = {
+      N: 2 ** 14, // trabajo (CPU/memoria)
+      r: 8, // bloques
+      p: 1, // paralelismo
+      keyLen: 32, // longitud de la clave
+    }
+  }
+
+  encrypt(message: string): string {
+    /** genera un salt aleatoria de 16 caracteres */
+    const salt = crypto.randomBytes(this.AES_BLOCK_SIZE)
+    const privateKey = crypto.scryptSync(passPhrase, salt, this.SCRYPT_OPTIONS.keyLen, {
+      cost: this.SCRYPT_OPTIONS.N,
+      blockSize: this.SCRYPT_OPTIONS.r,
+      parallelization: this.SCRYPT_OPTIONS.p,
+    })
+
+    /** genera un IV aleatorio (que sea aleatorio lo hace más seguro) */
+    const iv = crypto.randomBytes(this.AES_BLOCK_SIZE)
+    const cipher = crypto.createCipheriv('aes-256-gcm', privateKey, iv)
+    const cipherText = Buffer.concat([cipher.update(message, 'utf8'), cipher.final()])
+    const tag = cipher.getAuthTag()
+
+    /** genera el string encriptado */
+    const encryptedString = [cipherText.toString('base64'), salt.toString('base64'), iv.toString('base64'), tag.toString('base64')].join(
+      '*',
+    )
+
+    return encryptedString
+  }
+
+  decrypt(encryptedString: string): string | undefined {
+    logger.debug(encryptedString)
+    try {
+      /** separa el string en partes */
+      const [cipherTextB64, saltB64, nonceB64, tagB64] = encryptedString.split('*')
+
+      /** decodifica las partes de Base64 */
+      const cipherText = Buffer.from(cipherTextB64, 'base64')
+      const salt = Buffer.from(saltB64, 'base64')
+      const nonce = Buffer.from(nonceB64, 'base64')
+      const tag = Buffer.from(tagB64, 'base64')
+
+      const privateKey = crypto.scryptSync(passPhrase, salt, this.SCRYPT_OPTIONS.keyLen, {
+        cost: this.SCRYPT_OPTIONS.N,
+        blockSize: this.SCRYPT_OPTIONS.r,
+        parallelization: this.SCRYPT_OPTIONS.p,
+      })
+
+      const decipher = crypto.createDecipheriv('aes-256-gcm', privateKey, nonce)
+      decipher.setAuthTag(tag)
+
+      /** desencripta el texto */
+      const decrypted = Buffer.concat([decipher.update(cipherText), decipher.final()])
+
+      return decrypted.toString('utf8')
+    } catch (err: unknown) {
+      if (err instanceof Error) throw new Error('Error de desncriptación')
+      // throw new DecryptionException()
+      else throw err
+    }
+  }
+}
+
+export default new CryptoCode()
