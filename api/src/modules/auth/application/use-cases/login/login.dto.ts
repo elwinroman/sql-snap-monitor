13bcd4063c50d6ee0b87e1d3a3992d8fed8c5ef9
Elwin Roman <elwinromanv@gmail.com>
2025-05-19 22:02:43 -0500

    chore(api): remueve archivos eliminados

--- api/src/modules/auth/application/use-cases/login/login.dto.ts
+++
@@ -1,3 +0,0 @@
-import { StoreUserSchema } from '@shared/domain/store'
-
-export type LoginDto = StoreUserSchema
--- api/src/modules/auth/domain/ports/drivers/for-control-authenticating.port.ts
+++
@@ -1,8 +0,0 @@
-import { LoginDto } from '@auth/application/use-cases/login/login.dto'
-import { AuthenticatedUser } from '@auth/domain/schemas/auth-user'
-
-export interface ForControlAuthenticatingPort {
-  login(user: LoginDto): Promise<AuthenticatedUser>
-  logout(): void
-  health(): void
-}
