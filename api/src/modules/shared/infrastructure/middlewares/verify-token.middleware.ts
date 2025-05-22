e7bf5598019d65a1c6af49d9ed1f7dd290a691f2
Elwin Roman <elwinromanv@gmail.com>
2025-05-22 09:12:15 -0500

    feat(api): refactoriza middleware de verificación de token

---
+++ api/src/modules/shared/infrastructure/middlewares/verify-token.middleware.ts
@@ -0,0 +1,25 @@
+import { authenticatorProxyAdapter } from '@auth/infrastructure/adapters/drivers/proxies/composition-root'
+import { UnauthorizedException } from '@shared/domain/exceptions'
+import { setLoggerRequestContext } from '@shared/domain/logger-context'
+import { NextFunction, Request, Response } from 'express'
+
+import { extractBearerToken } from '@/modules/shared/infrastructure/utils/extract-bearer-token.util'
+
+/** Middleware que verifica la validez del token de acceso, que actúa como middleware intermediario. */
+export async function verifyTokenMiddleware(req: Request, _res: Response, next: NextFunction) {
+  // obtiene el access token desde el Bearer
+  const accessToken = extractBearerToken(req.headers.authorization)
+
+  if (!accessToken) throw new UnauthorizedException()
+
+  try {
+    const decodedToken = await authenticatorProxyAdapter.verifyToken(accessToken)
+    // agregar userId en el contexto del logger y el request
+    setLoggerRequestContext({ userId: decodedToken.user_id, role: decodedToken.role, jti: decodedToken.jti }, next)
+    req.userId = decodedToken.user_id
+
+    next()
+  } catch (err) {
+    next(err)
+  }
+}
